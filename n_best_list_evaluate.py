638838c1a3b6c419c5eb9d980a224ea60b6703cc
mattias421 <mcross2@sheffield.ac.uk>
2023-09-05 16:11:37 +0000

    after clean up

--- n_best_list_evaluate.py
+++ n_best_list_evaluate.py
@@ -14,31 +14,12 @@ def get_best_transcription(idx, n_best_list, alpha):
     reference = n_best_list[idx]['targets']
 
     # 'first_pass_score', 'am_score', 'bpe_lm_score', 'first_pass_length_penalty', 'ngram_lm_score_non_oov', 'ngram_lm_score_oov', 'ngram_lm_score', 'second_pass_score', 'diffusion_score'
-    scoring = lambda x : - 1 / (x['am_score'] + 1e-5)
     rescoring = lambda x : np.dot(alpha, list(x.values())[1:])
-    # rescoring = lambda x : x['diffusion_score'] + x['am_score'] + alpha[0] * x['bpe_lm_score'] + x['first_pass_length_penalty'] + alpha[1] * x['ngram_lm_score']
-    oracle = lambda x : wer(reference, x['text'])
-    random = lambda x : np.random.randint(1000)
 
     best_list = list(n_best_list[idx]['beams'][0].values())
-    # print(best_list[0].keys())
 
-    # original_list = sorted(best_list, key=scoring)
     new_list = sorted(best_list, key=rescoring)
-    # oracle_list = sorted(best_list, key=oracle)
-    # random_list = sorted(best_list, key=lambda x : np.random.randint(1000))
 
-    # original_wer = [wer(reference, t['text']) for t in original_list]
-    # new_wer = [wer(reference, t['text']) for t in new_list]
-    # oracle_wer = [wer(reference, t['text']) for t in oracle_list]
-
-    # print('original ', original_wer[0])
-    # print('new ', new_wer[0])
-    # print('oracle ', oracle_wer[0])
-
-    # print(rescoring(new_list[0]))
-
-    # return reference[0], best_list[0]['text'], oracle_list[0]['text'], original_list[0]['text'], new_list[0]['text'], random_list[0]['text']
     return reference[0], new_list[0]['text']
 
 
@@ -46,11 +27,7 @@ def get_best_transcription(idx, n_best_list, alpha):
 def calc_wer(n_best_list, n_samples):
 
     references = []
-    # first_beam = []
-    # oracles = []
-    # originals = []
     news = []
-    # randoms = []
 
     # 'first_pass_score', 'am_score', 'bpe_lm_score', 'first_pass_length_penalty', 'ngram_lm_score_non_oov', 'ngram_lm_score_oov', 'ngram_lm_score', 'second_pass_score', 'diffusion_score'
     alpha = np.random.normal(loc=0, scale=1, size=9)
@@ -60,17 +37,9 @@ def calc_wer(n_best_list, n_samples):
         ref, new = get_best_transcription(i, n_best_list, alpha)
 
         references.append(ref)
-        # first_beam.append(beam)
-        # oracles.append(ora)
-        # originals.append(ori)
         news.append(new)
-        # randoms.append(rand)
-    
-    # print(f'Oracle {wer(references, oracles)}')
-    # print(f'First beam {wer(references, first_beam)}')
-    # print(f'Originals {wer(references, originals)}')
+
     print(f'New {wer(references, news)}')
-    # print(f'Random {wer(references, randoms)}')
 
 
     return np.append(alpha, wer(references, news))
@@ -84,7 +53,6 @@ def main(args):
     results = np.genfromtxt(args.file, delimiter=',').reshape((len(n_best_list), N))
 
     print(results.shape)
-    # print([n_best_list[100]['beams'][0][i]['second_pass_score'] for i in range(N)])
 
     n_samples = len(n_best_list)
 
